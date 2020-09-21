b0c7853de90f9ddfa00983cec2b8e34635d46995
d7p4n4 <bercikezs199999999@gmail.com>
2020-09-21 16:56:35 +0200

    Add project files.

---
+++ CSClassLibForJavaOData/Cars.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSClassLibForJavaOData
+{
+    public class Cars
+    {
+        public int Id { get; set; }
+        public string Manufacture { get; set; }
+        public string Type { get; set; }
+        public string color { get; set; }
+    }
+}
---
+++ CSClassLibForJavaOData/Colors.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSClassLibForJavaOData
+{
+    public class Colors
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
